88fb5680c5df47e9ae5cf7cb69fdf976b0cb0fce
Slfdspln <121422214+Slfdspln@users.noreply.github.com>
2023-06-23 16:13:52 -0700

    updates

--- public/js/newPostHandler.js
+++ public/js/newPostHandler.js
@@ -6,7 +6,7 @@ async function newPostHandler(event) {
   const description = document.querySelector("#bodyInput").value.trim();
 
   if (title && description) {
-    const response = await fetch(`/api/blogPost`, {
+    const response = await fetch(`/api/blogPost:1`, {
       method: "POST",
       body: JSON.stringify({
         title,
